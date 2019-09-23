ce90dafdb07c339c6d8cae0cbad1ac5a6e12c3a8
InhyeJeong <inhyejeong59@gmail.com>
2019-09-23 19:41:27 +0900

    App State Component, Stateless Componet
    Parent Component : Clock.js
    Child Component : AnalogClock, DigitalClock
    All of these child component are Stateless Component !

---
+++ my-app/src/Components/Clock.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import AnalogClock from './AnalogClock';
+class Clock extends Component {
+	constructor(props) {
+		super(props);
+    this.launchClock()
+    this.state = {
+      currentTime: (new Date()).toLocaleString('en')
+    }
+  }
+  launchClock () {
+    setInterval(() => {
+      this.setState({
+        //  매 초마다 현재시각으로 상태 갱신
+        currentTime: (new Date()).toLocaleString('en')
+      });
+    }, 1000);
+  }
+  render() {
+    //  componet로 분리해서 사용가능
+    const DigitalClock = function(props) {
+      return <div style={{left: '50%', position: 'relative'}}>{props.time}</div>
+    }
+    return (
+      <div>
+        <AnalogClock time={this.state.currentTime}/>
+        <DigitalClock time={this.state.currentTime}/>
+      </div>
+    )
+  }
+}
+
+export default Clock;
--- my-app/src/Components/UILayout/SupportLayout.js
+++ my-app/src/Components/UILayout/SupportLayout.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 import 'element-theme-default';
 import MainPage from '../MainPage';
+import Clock from '../Clock';
 import Sidebar from '../Sidebar';
 import '../../App';
 
 class SupportLayout extends Component {
 	render() {
 		return (
-			// <div className="">
-      //   <div className="">
-      //     <div>
-      //       <Sidebar></Sidebar>
-      //     </div>
-      //     <div className="">
-      //       <MainPage></MainPage>
-      //     </div>
-      //   </div>
-      // </div>
-      <Sidebar></Sidebar>
+			<div className="">
+        <div className="">
+          <div>
+            <Sidebar></Sidebar>
+          </div>
+          <div className="">
+            <Clock></Clock>
+            {/* <MainPage></MainPage> */}
+          </div>
+        </div>
+      </div>
 		);
 	}
 }
