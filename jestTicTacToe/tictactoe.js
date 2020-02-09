f83e69a2fe6856492fd68a065dea36193e54cf3b
InhyeJeong <inhyejeong59@gmail.com>
2020-02-09 15:55:15 +0900

    [TicTacToe Kata TDD] Test A player Take One Field
    1. Change string items to boolean items in filed Array
    2. Add Player Class

--- jestTicTacToe/tictactoe.js
+++ jestTicTacToe/tictactoe.js
@@ -4,25 +4,37 @@
 // - 이미 돌이 놓인 필드에는 돌을 놓을 수 없다.
 // [게임 종료 조건]
 // * 승리
-// - 한 플레이어가 한 행의 모든 필드에 돌을 놓았을 때
-// - 한 플레이어가 한 열의 모든 필드에 돌을 놓았을 때
-// - 한 플레이어가 한 대각선의 모든 필드에 돌을 놓았을 때
+// - 한 플레이어가 한 행의 모든 필드에 돌을 놓았을 때 ---> 012 / 345 / 678
+// - 한 플레이어가 한 열의 모든 필드에 돌을 놓았을 때 ---> 036 / 147 / 258
+// - 한 플레이어가 한 대각선의 모든 필드에 돌을 놓았을 때 ---> 048 / 247
 // * 무승부
 // - 마지막 필드에 돌이 놓일 때
 class TicTacToe {
   constructor () {
-    this.fieldArray = Array(9).fill("")
-  }
-  takeField () {
-    let randomNumber = Math.floor(Math.random() * 10)
-    if (this.fieldArray[randomNumber] !== 'X') {
-      this.fieldArray[randomNumber] = 'X'
+    this.fieldArray = Array(9).fill(false)
+    this.players = {
+      0: new Player('Player A'),
+      1: new Player('Player B'),
     }
+    // [x,x,x
+    // o,o,o,
+    // x,x,x]
+  }
+  takeField (playerId) {
+    //  1) 본인 차례마다 패를 놓고, 
+    let randomNumber = Math.floor(Math.random() * 10)
+    if (this.fieldArray[randomNumber] !== true) {
+      this.fieldArray[randomNumber] = true
+    }
+    //  2) 그 인덱스를 해당 플레이어에 저장한다.
+    const player = this.players[playerId]
+    player.takePoint(randomNumber)
+    //  3) 그 인덱스가 win규칙에 해당되면 winner가 된다
   }
   getCurrentField () {
     let currentFieldLength = this.fieldArray.length
     for (let i = 0; i < this.fieldArray.length; i ++) {
-      if (this.fieldArray[i] === 'X') {
+      if (this.fieldArray[i] === true) {
         currentFieldLength = currentFieldLength - 1
       }
     }
@@ -30,4 +42,20 @@ class TicTacToe {
   }
 }
 
-module.exports = TicTacToe
+class Player {
+  constructor (name) {
+    this.name = name
+    this.fieldIndexList = []
+  }
+  takePoint (fieldIndex) {
+    this.fieldIndexList.push(fieldIndex)
+  }
+  getName () {
+    return this.name
+  }
+}
+
+module.exports = {
+  TicTacToe,
+  Player,
+}
--- jestTicTacToe/tictactoe.test.js
+++ jestTicTacToe/tictactoe.test.js
@@ -1,9 +1,11 @@
-let TicTacToe = require('./tictactoe')
+const { TicTacToe, Player } = require('./tictactoe')
 let tictactoe
 beforeEach(() => tictactoe = new TicTacToe())
 describe('Test TicTacToe', () => {
-  test('Take One Field', () => {
-    tictactoe.takeField()
+  const PLAYER_A_ID = 0
+  const PLAYER_B_ID = 1
+  test('A player Take One Field', () => {
+    tictactoe.takeField(PLAYER_A_ID)
     expect(tictactoe.getCurrentField()).toBe(8)
   })
 })
